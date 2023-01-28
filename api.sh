747b316e3782254b6fea7d7ceacbddf1562332c1
littlesum <impkt13@gmail.com>
2023-01-28 15:08:52 +0800

    truenas api check script

---
+++ api.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+# this is a truenas api check script
+#truenas api token
+#1-i8tQ0guckADx1pbv4xsFwoP8vpmEUbq3Ztn1trOb8aZWApRDYhmFb4LzkBnyTpTU
+#
+curl -X POST -H "Authorization: Bearer token" \
+ 	"https://yourtruenas.com/api/v2.0/certificate" \ 
+	 -k -vv -d '{"var1":data1,"var2":data2}'
